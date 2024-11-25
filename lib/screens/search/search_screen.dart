658990522f5a801cc620e6a220f3af21fac0fa5f
Daemon Blackfyre <akpabiosamuel2022@gmail.com>
2024-11-25 16:33:56 +0100

    a little finishing touches

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:ticket_app/base/res/media.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/widgets/app_double_text.dart';
 import 'package:ticket_app/screens/search/widgets/app_text_icon.dart';
 import 'package:ticket_app/screens/search/widgets/app_ticket_tabs.dart';
 import 'package:ticket_app/screens/search/widgets/find_tickets.dart';
+import 'package:ticket_app/screens/search/widgets/ticket_promotion.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppStyles.bgColor,
       body: ListView(
@@ -57,119 +56,7 @@ class SearchScreen extends StatelessWidget {
           const SizedBox(
             height: 15,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                width: size.width * .42,
-                height: 435,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.shade200,
-                          blurRadius: 1,
-                          spreadRadius: 2)
-                    ]),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 190,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          image: const DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage(AppMedia.planeSit))),
-                    ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    Text(
-                      "20% discount on the early booking of this flight, don't miss it!",
-                      style: AppStyles.headLineStyle2,
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                children: [
-                  Stack(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 15, horizontal: 15),
-                        width: size.width * .44,
-                        height: 210,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF3Ab8b8),
-                          borderRadius: BorderRadius.circular(18),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Discount\nfor survey',
-                              style: AppStyles.headLineStyle2.copyWith(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              'Take the survey about our services and get a discount',
-                              style: AppStyles.headLineStyle2.copyWith(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 18,
-                                  color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Positioned(
-                        right: -45,
-                        top: -40,
-                        child: Container(
-                          padding: const EdgeInsets.all(30),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              width: 18,
-                              color: AppStyles.circleColor,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 15, horizontal: 15),
-                    width: size.width * .44,
-                    height: 210,
-                    decoration: BoxDecoration(
-                        color: const Color(0xFFEC6545),
-                        borderRadius: BorderRadius.circular(18)),
-                    child: Column(
-                      children: [
-                        Text(
-                          'Take love',
-                          style: AppStyles.headLineStyle2
-                              .copyWith(color: Colors.white),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ],
-          )
+          const TicketPromotion(),
         ],
       ),
     );
