e6a572cd57642947d4b41cdf55221bb45f26dda3
Daemon Blackfyre <akpabiosamuel2022@gmail.com>
2024-11-28 12:18:09 +0100

    made a few changes and blah blah blah

--- lib/base/res/media.dart
+++ lib/base/res/media.dart
@@ -3,4 +3,5 @@ class AppMedia{
   static const logo = '$_baseImage/logo.png';
   static const hotelRoom = '$_baseImage/hotel_room.jpg';
   static const planeSit = '$_baseImage/plane_sit.jpg';
+  static const visaCard = '$_baseImage/visa_card.jpg';
 }
--- lib/screens/ticket/ticket_screen.dart
+++ lib/screens/ticket/ticket_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/media.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/base/res/utils/app_json.dart';
+import 'package:ticket_app/base/widgets/app_column_text_layout.dart';
+import 'package:ticket_app/base/widgets/app_layout_builder_widget.dart';
 import 'package:ticket_app/base/widgets/ticket_view.dart';
 import 'package:ticket_app/screens/search/widgets/app_ticket_tabs.dart';
 
@@ -29,8 +32,112 @@ class TicketScreen extends StatelessWidget {
             height: 20,
           ),
           Container(
-              padding: const EdgeInsets.only(left: 16),
-              child: TicketView(ticket: ticketList[0], isColor: true,)),
+            padding: const EdgeInsets.only(left: 16),
+            child: TicketView(
+              ticket: ticketList[0],
+              isColor: true,
+            ),
+          ),
+          const SizedBox(
+            height: 1,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+            decoration: BoxDecoration(
+              color: AppStyles.ticketColor,
+            ),
+            child: Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppColumnTextLayout(
+                      topText: 'Flutter DB',
+                      bottomText: 'Passenger',
+                      alignment: CrossAxisAlignment.start,
+                      isColor: true,
+                    ),
+                    AppColumnTextLayout(
+                      topText: '5221 36869',
+                      bottomText: 'Passport',
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const AppLayoutBuilderWidget(
+                  randomDivider: 15,
+                  width: 5,
+                  isColor: false,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppColumnTextLayout(
+                      topText: '2465 65859537340',
+                      bottomText: 'Number of E-ticket',
+                      alignment: CrossAxisAlignment.start,
+                      isColor: true,
+                    ),
+                    AppColumnTextLayout(
+                      topText: 'B46895',
+                      bottomText: 'Booking Code',
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const AppLayoutBuilderWidget(
+                  randomDivider: 15,
+                  width: 5,
+                  isColor: false,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              AppMedia.visaCard,
+                              height: 40,
+                              width: 50,
+                            ), //main scale to use with actual Visa Logo is #11
+                            Text(
+                              ' *** 2462',
+                              style: AppStyles.headLineStyle3,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 5,),
+                        Text('Payment Method', style: AppStyles.headLineStyle4,)
+                      ],
+                    ),
+                    const AppColumnTextLayout(
+                      topText: '\$249.99',
+                      bottomText: 'Price',
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
