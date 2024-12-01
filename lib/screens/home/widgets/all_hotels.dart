187d6add98934212173164285c1bb54a6cfda203
Daemon Blackfyre <akpabiosamuel2022@gmail.com>
2024-12-01 15:58:29 +0100

    Made the hotel pages to be more beautiful AND responsive

--- lib/screens/home/widgets/all_hotels.dart
+++ lib/screens/home/widgets/all_hotels.dart
@@ -28,7 +28,7 @@ class AllHotels extends StatelessWidget {
             itemCount: hotelList.length,
             itemBuilder: (context, index) {
               var singleHotel = hotelList[index];
-              return HotelGridView(hotel: singleHotel);
+              return HotelGridView(hotel: singleHotel, index:index);
             },
           ),
         ),
@@ -39,13 +39,17 @@ class AllHotels extends StatelessWidget {
 
 class HotelGridView extends StatelessWidget {
   final Map<String, dynamic> hotel;
-  const HotelGridView({super.key, required this.hotel});
+  final int index;
+  const HotelGridView({super.key, required this.hotel, required this.index});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.pushNamed(context, AppRoutes.hotelDetail);
+        print("my index is $index");
+        Navigator.pushNamed(context, AppRoutes.hotelDetail, arguments: {
+          "index": index
+        });
       },
       child: Container(
         padding: const EdgeInsets.all(10),
--- lib/screens/hotel_detail.dart
+++ lib/screens/hotel_detail.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/styles/app_styles.dart';
+import 'package:ticket_app/base/res/utils/all_json.dart';
 
-class HotelDetail extends StatelessWidget {
+class HotelDetail extends StatefulWidget {
   const HotelDetail({super.key});
+
+  @override
+  State<HotelDetail> createState() => _HotelDetailState();
+}
+
+class _HotelDetailState extends State<HotelDetail> {
+  late int index;
+
+  @override
+  void didChangeDependencies() {
+    var args = ModalRoute.of(context)!.settings.arguments as Map;
+    print(args["index"]);
+    index = args['index'];
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +29,65 @@ class HotelDetail extends StatelessWidget {
             expandedHeight: 300,
             floating: false,
             pinned: true,
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: AppStyles.primaryColor,
+                  ),
+                  child: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
             flexibleSpace: FlexibleSpaceBar(
-                title: const Text('Hotel Title'),
-                background:
-                    Image.network('https://via.placeholder.com/600x400')),
+                //title: Text(hotelList[index]['place']),
+                background: Stack(
+              children: [
+                Positioned.fill(
+                  child: Image.asset(
+                    "assets/images/${hotelList[index]['image']}",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 20,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    color: Colors.black.withOpacity(0.5),
+                    child: Text(
+                      hotelList[index]['place'],
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 10.0,
+                            color: AppStyles.primaryColor,
+                            offset: const Offset(2.0, 2.0),
+                          )
+                        ]
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )),
           ),
           SliverList(
             delegate: SliverChildListDelegate(
               [
-                const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                      'In one of London\'s business districts, 2 miles from the World Trade Center, this hotel offers a free shuttle to the beach and shopping malls.Holiday Inn Express Dubai’s rooms are decorated in sandy brown tones and fitted with deluxe bedding. The living area features a loose-back sofa and TV with access to multilingual channels.At Holiday Inn Dubai, the front desk is open throughout the day, and the attentive staff can organize outdoor excursions, including desert safaris.Guests can order light snacks and sandwiches at the Lobby bar. It offers music entertainment and a range of wines and draft beers. Warm drinks, smoothies and fruit drinks made from fresh ingredients are available. A hot buffet breakfast is served daily.Holiday Inn Express Dubai is a 20 minute drive from Dubai Airport.'),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(hotelList[index]["detail"]),
                 ),
                 const Padding(
                   padding: EdgeInsets.all(16.0),
